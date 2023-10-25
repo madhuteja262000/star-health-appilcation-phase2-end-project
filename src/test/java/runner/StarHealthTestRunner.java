93cc3158c16994a3b91631f51acfc943999d4e4f
madhutejaswara reddy <madhutejaswara reddy@madhu>
2023-10-25 14:10:39 +0530

    first commit

---
+++ src/test/java/runner/StarHealthTestRunner.java
@@ -0,0 +1,18 @@
+package runner;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\madhutejaswara reddy\\eclipse-workspace\\endprojecthealth\\src\\test\\java\\features",
+				glue= {"steps"},
+				plugin= {"html:target/Cucumberreport.html",
+						"pretty",
+						"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+						"timeline:test-output-thread/"		
+})
+
+public class StarHealthTestRunner {
+	
+}
---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"7120a333-7da7-48a6-a470-b71ceab2e965","feature":"Test the starHealth page on Chrome Browser","scenario":"Validate the Star Health Buy Now flow","start":1698156730610,"group":1,"content":"","tags":"","end":1698156786432,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
